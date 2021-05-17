6df87e4e7070b50dd7b6ccac5e04d3714782681b
leandro <leandronegreiros07@gmail.com>
2021-05-17 14:50:51 -0400

    Criando Dashboard e aplicando conceitos do styled-componentes

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,59 @@
 import React from "react";
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={ logoImg } alt="Github Explore" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img 
+                        src="https://avatars.githubusercontent.com/u/31863898?v=4" 
+                        alt="Leandro Negreiros" 
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS e React forms!</p>
+                    </div>
+
+                     <FiChevronRight size={ 20 } /> 
+                </a>
+                <a href="teste">
+                    <img 
+                        src="https://avatars.githubusercontent.com/u/31863898?v=4" 
+                        alt="Leandro Negreiros" 
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS e React forms!</p>
+                    </div>
+
+                     <FiChevronRight size={ 20 } /> 
+                </a>
+                <a href="teste">
+                    <img 
+                        src="https://avatars.githubusercontent.com/u/31863898?v=4" 
+                        alt="Leandro Negreiros" 
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS e React forms!</p>
+                    </div>
+
+                     <FiChevronRight size={ 20 } /> 
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
