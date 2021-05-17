ce4139352175145f0b99bb9c5a5372cdd0a97b4c
leandro <leandronegreiros07@gmail.com>
2021-05-17 12:48:45 -0400

    add pacote styled-componets

--- 05-primeiro-projeto-react/src/App.tsx
+++ 05-primeiro-projeto-react/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyle from './styles/global'
 import { BrowserRouter } from 'react-router-dom';
 
+
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
