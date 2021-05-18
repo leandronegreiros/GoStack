fe9f2f224bbd4ee4c556e796178ecb37b55dc3ff
leandro <leandronegreiros07@gmail.com>
2021-05-18 13:44:28 -0400

    Navegando entre rotas

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from "react";
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg'
 import { FiChevronRight } from 'react-icons/fi'
 import api from '../../services/api'
@@ -18,7 +19,9 @@ const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
     const [repositories, setRepositories] = useState<Repository[]>(() => {
-        const storagedRepositories = localStorage.getItem('@GithubExplore:repositories');
+        const storagedRepositories = localStorage.getItem(
+            '@GithubExplore:repositories'
+        );
 
         if (storagedRepositories) {
             return JSON.parse(storagedRepositories);
@@ -73,7 +76,9 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} 
+                        to={`/repositories/${repository.full_name}`}
+                    >
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -84,7 +89,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
