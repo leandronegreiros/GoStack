01c97bed907d2b6c4cabef10b2a56fd563e4d698
leandro <leandronegreiros07@gmail.com>
2021-06-17 10:08:58 -0400

    Usabilidade do input
    iremos melhorar ainda mais a usabilidade dos inputs, como por exemplo mudar a cor do ícone e adicionar uma borda ao receber foco para indicar ao usuário qual input está selecionado. Além disso, vamos conhecer também um novo hook: o useCallback.

--- 06-gobarber-web/src/components/inpput/styles.ts
+++ 06-gobarber-web/src/components/inpput/styles.ts
@@ -1,19 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+}
 
 // eslint-disable-next-line import/prefer-default-export
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
     background: #232129;
     border-radius: 10px;
-    border: 2px solid #232129;
     padding: 16px;
     width: 100%;
+
+    border: 2px solid #232129;
     color: #666360;
+
     display: flex;
     align-items: center;
 
     & + div {
         margin-top: 8px;
     }
+
+    ${props =>
+        props.isFocused &&
+        css`
+            color: #ff9000;
+            border-color: #ff9000;
+        `}
+
+    ${props =>
+        props.isFilled &&
+        css`
+            color: #ff9000;
+        `}
+
     input {
         flex: 1;
         background: transparent;
