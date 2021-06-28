c1d06f7468c2b49a426528c3415185ead0474f99
leandro <leandronegreiros07@gmail.com>
2021-06-28 15:58:57 -0400

    Habilitando CORS na API
    remos começar conectar a aplicação com a parte da API que já foi desenvolvida, mas antes precisamos instalar na API um pacote de segurança chamado CORS. É ele quem define qual site pode acessar a nossa aplicação (você também pode definir para que qualquer site possa acessar)

--- iniciando-back-end/src/server.ts
+++ iniciando-back-end/src/server.ts
@@ -1,4 +1,8 @@
+import 'express-async-errors';
+
 import 'reflect-metadata';
+
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -11,6 +15,7 @@ import { tr } from 'date-fns/locale';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
